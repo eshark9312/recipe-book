28d11c5c9636f8c7a1f6d18572583d678fc42ca9
cshark <cshark@dev.dom>
2023-03-27 02:05:41 -0700

    086 headerNav-alive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-courseProj-basic';
+  loaded:string = 'recipe';
+
+  onNavigate(feature: string){
+    this.loaded = feature;
+  }
 }
