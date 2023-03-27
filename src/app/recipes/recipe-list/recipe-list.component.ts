0522fae18fed763defbbdd6598bbd74fb1a4867f
cshark <cshark@dev.dom>
2023-03-27 03:15:47 -0700

    088 recipe-detail alive

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,13 +9,18 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent {
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'this is a test recipe', '../../../assets/imgs/xxx.png'),
-    new Recipe('A test recipe', 'this is a test recipe', '../../../assets/imgs/xxx.png')
+    new Recipe('Another test recipe', 'this is a test recipe', '../../../assets/imgs/xxx.png')
   ];
+  @Output() recipeWasSelected= new EventEmitter<Recipe>();
 
   constructor () {
 
   }
   ngOnInit () {
 
+  }
+
+  updateDetail(recipe2disp:Recipe){
+    this.recipeWasSelected.emit(recipe2disp)
   }
 }
