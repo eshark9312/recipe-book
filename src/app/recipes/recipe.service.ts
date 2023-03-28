6e0c8c82aead928fb32b2f93185e23b584425407
cshark <cshark@dev.dom>
2023-03-28 08:55:47 -0700

    123 services final

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,12 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
+@Injectable()
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
-    
+
     private recipes: Recipe[] = [
         new Recipe(
           'Tasty Schnitzel',
@@ -21,12 +23,15 @@ export class RecipeService{
             new Ingredient('Buns', 2),
             new Ingredient('Meat', 1)
           ])
-      ];
+    ];
 
+    constructor(private slService:ShoppingListService){}
+    
     getRecipes() {
         return this.recipes.slice();
     }
 
-
-
+    addIngredientsToShoppingList(ingredients : Ingredient[]){
+        this.slService.addIngredients(ingredients);
+    }
 }
