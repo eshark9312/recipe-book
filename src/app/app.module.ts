38adab4f874919d88bf7272df28fe916e4c3aac5
cshark <cshark@dev.dom>
2023-04-13 02:05:51 +0300

    deploy retry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { RecipesEffects } from './recipes/store/recipes.effects';
 import { AuthEffects } from './auth/store/auth.effects';
 import { appReducer } from './store/app.reducer';
-import { NgModule } from '@angular/core';
+import { InjectionToken, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SharedModule } from './shared/shared.module';
+
+const APP_BASE_HREF = null as InjectionToken<string>;
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { SharedModule } from './shared/shared.module';
     //StoreRouterConnectingModule.forRoot(),
     EffectsModule.forRoot([AuthEffects, RecipesEffects])
   ],
+  providers: [{provide: APP_BASE_HREF, useValue: '/recipe-book/'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
